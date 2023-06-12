116cbc890c6c6e08d67ee2da0151d283129c2758
admin <admin@DESKTOP-6AS9MCG>
2023-06-12 17:44:50 +0900

    20230612 최종수정.

--- src/com/yedam/memo/FileEx.java
+++ src/com/yedam/memo/FileEx.java
@@ -5,9 +5,19 @@ public class FileEx {
 
 	public static void main(String[] args) {
 		try {
-			FileOutputStream fos = new FileOutputStream("c:/temp/sample.dat");
-			fos.write(10);
-			fos.close();
+//			FileOutputStream fos = new FileOutputStream("c:/temp/sample.dat");
+//			fos.write(10);
+//			fos.close();
+			InputStream is = new FileInputStream("C:\\dev\\workspace\\20230609\\src\\com\\yedam\\memo\\MemoApp.java");
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String str = "";
+			while((str = br.readLine()) != null) {				
+				System.out.println(str);
+			}
+			br.close();
+			isr.close();
+			is.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
